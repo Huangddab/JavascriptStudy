e57c752d9524e96f925698427aaa398f8c224b60
huang-dandd <huangddab@163.com>
2025-03-13 22:28:01 +0800

    delete other

--- codewhy/javascript/example/12_获取属性描述符.js
+++ codewhy/javascript/example/12_获取属性描述符.js
@@ -1,13 +1,12 @@
 var obj = {
-    name:"what",
-    age:18
-}
+  name: "what",
+  age: 18,
+};
 
-
-
+// 禁止对象继续添加新的属性
+Object.preventExtensions(obj);
 // ，未禁止时打印{ name: 'what', age: 18, height: 1.88, width: 2.33 }
-obj.height = 1.88
-obj.width=2.33
+obj.height = 1.88;
+obj.width = 2.33;
 
-
-console.log(obj)
+console.log(obj);
